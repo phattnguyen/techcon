420f0d3ac8ee41819bb7d5c138a4b3633a9686a0
phattnguyen <n.phat@elsevier.com>
2022-12-28 21:19:23 +0700

    update script

--- cypress/e2e/demo/google.search.fail.spec.js
+++ cypress/e2e/demo/google.search.fail.spec.js
@@ -3,7 +3,7 @@ describe('Perform google search', () => {
     it('Will be failed', { tags: ['smoke'] }, () => {
       cy.visit('https://www.google.com/')
       cy.get('input[name="q"]').type('ABC{enter}')
-      cy.get('h2').contains('ABCCC Songs for Children - 13 Alphabet Songs & 26 Videos').should('exist')
+      cy.get('h2').contains('ABCCCC Songs for Children - 13 Alphabet Songs & 26 Videos').should('exist')
     });
   });
   
